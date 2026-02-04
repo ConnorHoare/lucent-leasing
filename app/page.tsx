8fec769dd141c33c2b643f449d28b3f6b24c981b
ConnorHoare <connor@tipcigroup.com>
2026-02-04 16:20:22 +0000

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import HomeHero from "@/components/Hero/HomeHero";
 
 export default function Home() {
   return (
     <div>
+      <HomeHero media={{ imageSrc: '/images/hero2.jpg', imageAlt: 'Lucent Leasing home hero' }} eyebrow="LUCENT LEASING" background={{ overlayStrength: 35, src: '/images/hero.jpg' }} />
     </div>
   );
 }
